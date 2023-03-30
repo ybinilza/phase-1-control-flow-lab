707cebd3ef09070df55e3b647a6edce368ba4e48
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-30 00:09:48 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+function scuberGreetingForFeet(someValue)
+{
+  
+  if (someValue <= 400) 
+  {
+    return ('This one is on me!');
+  }
+  else if (someValue >= 400 && someValue <=2000)
+  {
+    return('That will be twenty bucks.');
+  }
+  else if (someValue>2000 && someValue <= 2500)
+  {
+    return('I will gladly take your thirty bucks.');
+    
+  }
+  else if (someValue>2500)
+  {
+    return('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someValue)
+{
+  return(someValue ==='NYC'? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someValue)
+{
+  switch(someValue)
+  {
+    case 'generous':
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+      return('Thank you.');
+      break;
+    default:
+      return('Bye.');
+      break;
+  }
 }
